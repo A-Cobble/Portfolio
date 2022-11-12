0b0746761902060ba9966d0c61c6c768cd804782
A-Cobble <alex.cobble42@gmail.com>
2022-11-11 22:01:28 -0800

    changed image sizes to be smaller

--- public/images/index.js
+++ public/images/index.js
@@ -1,5 +1,5 @@
 import slide1 from "./Instructor-Hub.png";
-import slide2 from "./Steam-Clone.png";
+import slide2 from "./Steam-Clone1.png";
 import slide3 from "./Asteroids-Game.png";
 import slide4 from "./Dungeons & Dragons Character Sheet.png";
 
